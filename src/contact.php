4761a681545ecdd571ccbd65b40ff29ab0d54c54
Jonas <jonas@bulik.dev>
2025-01-09 20:26:19 +0100

    feat(contact): add captcha
    to prevent spam

--- src/contact.php
+++ src/contact.php
@@ -1,32 +1,65 @@
 <?php
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $phone = $_POST["phone"];
-  $company = $_POST["company"];
-  $message = $_POST["message"];
+$ini = parse_ini_file("/home/eleverch/.php-env");
+$hcaptcha_verify_url = 'https://api.hcaptcha.com/siteverify';
+$data = [
+  'response' => $_POST["h-captcha-response"],
+  'secret' => $ini["HCAPTCHA_SECRET"],
+  'remoteip' => $_SERVER['REMOTE_ADDR'],
+  'sitekey' => 'f1a24450-5f46-43b6-93f8-a6dec6746c76'
+];
+$options = [
+  'http' => [
+    'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+    'method' => 'POST',
+    'content' => http_build_query($data),
+  ],
+];
+$context = stream_context_create($options);
+$response = file_get_contents($hcaptcha_verify_url, false, $context);
+$responseJson = json_decode($response);
+$success = $responseJson->{'success'};
+
+if (!$success) {
+  echo "THIS DID NOT WORK. Please try again.";
+  return;
+}
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$company = $_POST["company"];
+$message = $_POST["message"];
 
 
-  $emailMessage = [];
-  $emailMessage[] = "Name: $name";
-  $emailMessage[] = "E-mail: $email";
-  $emailMessage[] = "Phone: $phone";
-  $emailMessage[] = "Company: $company";
-  $emailMessage[] = "Message: $message";
+$emailMessage = [];
+$emailMessage[] = "Name: $name";
+$emailMessage[] = "E-mail: $email";
+$emailMessage[] = "Phone: $phone";
+$emailMessage[] = "Company: $company";
+$emailMessage[] = "Message: $message";
 
-  // SEND MAIL
-  $fromUser = 'system@altusforge.com';
-  $to = 'th@altusforge.com';
-  $subject = 'New Contact from elever.ch';
-  $headers = [];
-  $headers[] = "From: $fromUser <$fromUser>";
-  $headers[] = "MIME-Version: 1.0";
-  $headers[] = "Content-type: text/html; charset=UTF-8";
+// SEND MAIL
+$fromUser = 'system@altusforge.com';
+$to = 'th@altusforge.com';
+$subject = 'New Contact from elever.ch';
+$headers = [];
+$headers[] = "From: $fromUser <$fromUser>";
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/html; charset=UTF-8";
 
-  mail($to, $subject, implode("<br/>", $emailMessage), implode("\n", $headers));
+$success = mail($to, $subject, implode("<br/>", $emailMessage), implode("\n", $headers));
 ?>
 
 <html>
-  <body>
-    Thank you for your message
-  </body>
+
+<body>
+  <div>
+    <?php if ($success): ?>
+      <p>Thank you for contacting us</p>
+    <?php else: ?>
+      <p>That didn't work. Please try again.</p>
+    <?php endif ?>
+  </div>
+</body>
+
 </html>
